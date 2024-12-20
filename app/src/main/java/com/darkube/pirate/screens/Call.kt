3383670bb3f2fb38d9f9f6c339a07edcbad85183
sandy-5000 <sandeepkumarbhaviri@gmail.com>
2024-12-20 14:17:05 +0530

    init

---
+++ app/src/main/java/com/darkube/pirate/screens/Call.kt
@@ -0,0 +1,28 @@
+package com.darkube.pirate.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Call(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier.fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Calls Tab!",
+                modifier = modifier
+            )
+        }
+    }
+}
