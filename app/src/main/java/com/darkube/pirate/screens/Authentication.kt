c426e83f680f21065f211f75541e597fa7a0b449
sandy-5000 <sandeepkumarbhaviri@gmail.com>
2025-01-25 20:35:35 +0530

    added scroll

--- app/src/main/java/com/darkube/pirate/screens/Authentication.kt
+++ app/src/main/java/com/darkube/pirate/screens/Authentication.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -54,7 +55,8 @@ fun Authentication() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = backgroundColor),
+            .background(color = backgroundColor)
+            .imePadding(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
