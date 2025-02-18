e40240ac8389618efe6b9aaf5a72d5c4616523a8
Hema-Juturu <jhema7834@gmail.com>
2025-02-18 22:30:42 +0530

    add invite friends

--- app/src/main/java/com/darkube/pirate/screens/InviteFriends.kt
+++ app/src/main/java/com/darkube/pirate/screens/InviteFriends.kt
@@ -1,7 +1,6 @@
 package com.darkube.pirate.screens
 
 import android.content.Intent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -21,7 +20,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -48,20 +46,28 @@ fun InviteFriends(
     val shareIntent = Intent.createChooser(sendIntent, null)
 
 
-    Column(modifier = modifier.fillMaxWidth().fillMaxHeight().padding(start = 50.dp).padding(top=20.dp)){
-        OutlinedTextField( value = shareLink, onValueChange = {shareLink=it},
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(start = 50.dp)
+            .padding(top = 20.dp)
+    ) {
+        OutlinedTextField(
+            value = shareLink, onValueChange = { shareLink = it },
             placeholder = { Text("Invite Friends") },
             shape = RoundedCornerShape(4.dp)
-            )
+        )
         Spacer(Modifier.height(12.dp))
-        Button(onClick = {
-            context.startActivity(shareIntent)
-        },
+        Button(
+            onClick = {
+                context.startActivity(shareIntent)
+            },
             colors = ButtonDefaults.buttonColors(
                 containerColor = PrimaryColor,
             ),
             shape = RoundedCornerShape(4.dp)
-            ) {
+        ) {
             Icon(
                 painter = painterResource(id = shareIcon),
                 contentDescription = "Share",
@@ -69,9 +75,10 @@ fun InviteFriends(
                 modifier = Modifier.size(20.dp),
             )
             Spacer(modifier = Modifier.width(4.dp))
-            Text("Share",
+            Text(
+                "Share",
                 color = Color.White,
-                )
+            )
         }
     }
 }
