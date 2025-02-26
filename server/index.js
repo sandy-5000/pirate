5f59c8ab0604de6194df3666856e6d092aa7bdb2
sandy-5000 <sandeepkumarbhaviri@gmail.com>
2025-02-26 18:02:31 +0530

    added socket code

--- server/index.js
+++ server/index.js
@@ -3,14 +3,20 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import admin from 'firebase-admin'
+import http from 'http'
+import { Server as SocketServer } from 'socket.io'
 import { readFileSync } from 'fs'
 import userController from '#~/controllers/user.controller'
 import pushTokenController from '#~/controllers/pushToken.controller'
 import friendsController from '#~/controllers/friends.controller'
 import { ERRORS } from '#~/utils/error.types'
+import { USER_STATUS } from '#~/utils/enums.constants'
 dotenv.config()
 
 const app = express()
+const server = http.createServer(app)
+const io = new SocketServer(server)
+
 app.use(express.json())
 app.use(cors())
 
@@ -39,10 +45,45 @@ app.use('*', (_, res) =>
   }),
 )
 
+const pirateIds = new Map()
+const userSockets = new Map()
+const onlineUsers = new Map()
+
+io.on('connection', (socket) => {
+  console.log('user connected:', socket.id)
+
+  socket.on('init', ({ pirateId }) => {
+    const prevSocket = userSockets.get(pirateId)
+    if (prevSocket) {
+      prevSocket.disconnect(true)
+      pirateIds.delete(prevSocket.id)
+    }
+    pirateIds.set(socket.id, pirateId)
+    userSockets.set(pirateId, socket)
+    onlineUsers.set(pirateId, { status: USER_STATUS.ONLINE })
+  })
+
+  socket.on('is-online', ({ otherPirateId }) => {
+    const userInfo = onlineUsers.get(otherPirateId)
+    const isOnline = userInfo?.status === USER_STATUS.ONLINE
+    socket.emit('is-online-response', { isOnline })
+  })
+
+  socket.on('disconnect', () => {
+    const pirateId = pirateIds.get(socket.id)
+    if (pirateId) {
+      onlineUsers.delete(pirateId)
+      userSockets.delete(pirateId)
+    }
+    pirateIds.delete(socket.id)
+    console.log('user disconnected:', socket.id)
+  })
+})
+
 mongoose
   .connect(MONGO_DB_URL)
   .then(() => {
     console.log('DB Connected')
-    app.listen(PORT, () => console.log(`visit http://${HOST}:${PORT}`))
+    server.listen(PORT, () => console.log(`visit http://${HOST}:${PORT}`))
   })
   .catch((e) => console.log('db NOT connected', e.message))
